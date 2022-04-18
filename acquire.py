a81181c5e2e02e53581c44d5b6cf9fba0ec2d28d
rachelrobbinsmayhill <rachel.robbinsmayhill@gmail.com>
2022-04-18 14:21:03 -0500

    Updates Stores Complete

--- acquire.py
+++ acquire.py
@@ -1,27 +1,150 @@
-# acquire.py
+'''
+This module contains all aspects of data acquisition for the sales, stores, and items endpoints within https://api.data.codeup.com/api/v1
 
+- get_store_item_demand_data() - will acquire all sales, store, and item data and merge it into one dataframe.
+
+If you want to get the individual datasets from the store item demand data you
+can do so with
+- get_items_data
+- get_stores_dat
+- get_sales_data
+
+- get_opsd_data() - will acquire all of the open power systems data for Germany and create a dataframe.
+'''
 import os
 import requests
 import pandas as pd
 
 
-'''
-This file contains functions to acquire the items, stores, and sales data from an api.
-
-it also contains a function to acquire the germany power data.
-'''
-
-
-
 def get_store_data_from_api():
-    '''
-    
-    '''
+
     response = requests.get('https://api.data.codeup.com/api/v1/stores')
     data = response.json()
-    tores = pd.DataFrame(data['payload']['stores'])
+    stores = pd.DataFrame(data['payload']['stores'])
     stores = pd.DataFrame(stores)
     return stores
+
+
+def get_items_data_from_api():    
+
+    domain = 'https://api.data.codeup.com'
+    # path or where we are accessing the data inside the url
+    endpoint = '/api/v1/items'
+    #create an empty list to place the data within
+    items = []
+
+    while endpoint is not None:
+        #assign url
+        url = domain + endpoint
+        # print(f'Acquiring page {endpoint}')
+        # making a request and storing the response to the request as a string
+        response = requests.get(url)
+        # taking the json data and converting to Pandas list of dictionaries (python)
+        data = response.json()
+        # .extend adds elements from a list to another list
+        items.extend(data['payload']['items'])
+        # reasigning the endpoint variable to have the path to the next page.
+        endpoint = data['payload']['next_page']
+
+    items = pd.DataFrame(items)
+    return items
+
+
+def get_sales_data_from_api():
+    
+    domain = 'https://api.data.codeup.com'
+    # path or where we are accessing the data inside the url
+    endpoint = '/api/v1/items'
+    #create an empty list to place the data within
+    sales = []
+    
+    while endpoint is not None:
+        #assign url
+        url = domain + endpoint
+        # print(f'Acquiring page {endpoint}')
+        # making a request and storing the response to the request as a string
+        response = requests.get(url)
+        # taking the json data and converting to Pandas list of dictionaries (python)
+        data = response.json()
+        # .extend adds elements from a list to another list
+        items.extend(data['payload']['items'])
+        # reasigning the endpoint variable to have the path to the next page.
+        endpoint = data['payload']['next_page']
+
+    items = pd.DataFrame(items)
+    return items
+
+
+
+def get_store_data():
+    
+    filename = 'stores.csv'
+
+    if os.path.exists(filename):
+        print('Reading from csv file...')
+        return pd.read_csv(filename)
+    else:
+        df = get_stores_data_from_api()
+        df.to_csv(filename)
+    return df   
+
+
+def get_items_data():
+    
+    filename = 'items.csv'
+
+    if os.path.exists(filename):
+        print('Reading from csv file...')
+        return pd.read_csv(filename)
+    else:
+        df = get_items_data_from_api()
+        df.to_csv(filename)
+    return df   
+
+
+def get_sales_data():
+    
+    filename = 'sales.csv'
+
+    if os.path.exists(filename):
+        print('Reading from csv file...')
+        return pd.read_csv(filename)
+    else:
+        df = get_sales_data_from_api()
+        df.to_csv(filename)
+    return df  
+
+
+def get_store_item_demand_data():
+    
+    sales = get_sales_data()
+    items = get_items_data()
+    stores = get_stores_data()
+    
+    sales = sales.rename(columns={'item': 'item_id', 'store': 'store_id'})
+    df = pd.merge(sales, items, how='left', on='item_id')
+    df = pd.merge(df, items, how='left', on='item_id')
+
+    return df
+
+
+
+
+
+def get_opsd_data():
+    if os.path.exists('opsd.csv'):
+        return pd.read_csv('opsd.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv')
+    df.to_csv('opsd.csv', index=False)
+    return df
+
+
+
+
+
+
+
+
 
 
 
@@ -52,6 +175,7 @@ def get_germany_power_data():
         opsd = pd.read_csv('opsd_germany_power_data.csv')
     print('Data acquisition complete.')
     return opsd
+
     
 
 
