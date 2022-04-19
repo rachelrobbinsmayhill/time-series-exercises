2b3a47e4b159632155eb818ddadd9da345a5f714
rachelrobbinsmayhill <rachel.robbinsmayhill@gmail.com>
2022-04-19 15:32:42 -0500

    Updated with Separate Prepare for Explore and Modeling

--- prepare.py
+++ prepare.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
 
+def prep_store_data_for_exploration(df):
+    df.sale_date = df.sale_date.apply(lambda date: date[:-13])
+    df.sale_date = pd.to_datetime(df.sale_date, format='%a, %d %b %Y')
+    df = df.set_index('sale_date').sort_index()
+    df = df.rename(columns={'sale_amount': 'quantity'})
+    df['month'] = df.index.month
+    df['day_of_week'] = df.index.day_name()
+    df['sales_total'] = df.quantity * df.item_price
+    return df
+
+
+
 def prep_store_data(df):
-    df.sale_date = pd.to_datetime(df.sale_date.apply(lambda x: x[:-13]))
+    df.sale_date = df.sale_date.apply(lambda date: date[:-13])
+    df.sale_date = pd.to_datetime(df.sale_date, format='%a, %d %b %Y')
     df = df.set_index('sale_date').sort_index()
     df = df.rename(columns={'sale_amount': 'quantity'})
     df['month'] = df.index.month
@@ -11,7 +24,7 @@ def prep_store_data(df):
     # ensure that our data is on a MONTH BY MONTH representation by agg/resample on day: sum
     df_resampled = df.resample('M')[['sales_total','quantity']].sum()
     # remove leap day for the sake of congruent seasonal analysis
-    df_resampled = df_resampled[df_resampled.index != '2016-02-29']
+    #df_resampled = df_resampled[df_resampled.index != '2016-02-29']
     return df_resampled
 
 def prep_opsd_data(df):
